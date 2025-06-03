8b4993f61853e9f304860b099c3bca7dc1ad6c9d
tk <trishank01@gmail.com>
2025-06-03 15:00:13 +0530

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ const {
   updateUserProfile,
 } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
-const upload = require("../middleware/uploadMiddleware");
+
 
 const router = express.Router();
 
@@ -16,25 +16,6 @@ router.post("/login", loginUser); //Login User
 router.get("/profile", protect, getUserProfile); //Get User Profile
 router.put("/profile", protect, updateUserProfile); //Update User Profile
 
-// router.post("/upload-image", upload.single("image"), (req, res) => {
-//   if (!req.file) {
-//     return res.status(400).json({ message: "No file uploaded" });
-//   }
 
-//   const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-//     req.file.filename
-//   }`;
-//   res.status(200).json({ imageUrl });
-// });
-
-
-//optional:
-router.post("/upload-image", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: "No file uploaded" });
-  }
-
-  res.status(200).json({ imageUrl: req.file.path }); // Cloudinary's secure URL
-});
 
 module.exports = router;
