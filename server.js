da5497409a6bf9519f3314376c51958106dceff7
tk <trishank01@gmail.com>
2026-05-17 13:56:42 +0530

    add backend dev ci pipeline

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.use(express.json())
       <body>
         <div class="container">
           <h1>Success!</h1>
-          <p>Your Node.js app is deployed and running correctly.</p>
+          <p>Your Node.js app is deployed and running correctly. version 1</p>
         </div>
       </body>
     </html>
